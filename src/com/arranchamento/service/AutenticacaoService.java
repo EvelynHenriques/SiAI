be38750a15f8f5126b82e465d63a36dcb04b112f
CarolGandolfi <caroline.gandolfi29@gmail.com>
2024-04-17 09:20:26 -0300

    cadastro de usuarios

--- src/com/arranchamento/service/AutenticacaoService.java
+++ src/com/arranchamento/service/AutenticacaoService.java
@@ -13,15 +13,10 @@ public class AutenticacaoService {
     public Usuario autenticar(String email, String senha) {
         // A senha deve ser um hash. Aqui, estamos assumindo que senha já é o hash.
         // Em um sistema real, você aplicaria um algoritmo de hash à senha antes de verificar.
-        Usuario usuario = usuarioDAO.buscarPorEmailESenha(email, senha);
 
-        if (usuario != null) {
-            // A senha fornecida corresponde à senha armazenada (hash).
-            return usuario;
-        } else {
-            // Autenticação falhou.
-            return null;
-        }
+        // A senha fornecida corresponde à senha armazenada (hash).
+        // Autenticação falhou.
+        return usuarioDAO.buscarPorEmailESenha(email, senha);
     }
 
     // Adicione aqui qualquer outro serviço relacionado à autenticação, como redefinição de senha, etc.
---
+++ src/com/arranchamento/servlet/CadastroServlet.java
@@ -0,0 +1,35 @@
+package arranchamento.servlet;
+
+import arranchamento.modelo.Usuario;
+import arranchamento.dao.UsuarioDAO;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "cadastroServlet", value = "/cadastroServlet")
+public class CadastroServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String nome = request.getParameter("nome");
+        String nomeDeGuerra = request.getParameter("nome_guerra");
+        int matricula = Integer.parseInt(request.getParameter("matricula"));
+        int pelotao = Integer.parseInt(request.getParameter("pelotao"));
+        int turma = Integer.parseInt(request.getParameter("turma"));
+
+        Usuario usuario = new Usuario(0, nome, email, password, nomeDeGuerra, matricula, turma, pelotao);
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        if(usuarioDAO.buscarPorEmailEMatricula(email, matricula) && usuarioDAO.inserirUsuario(usuario)){
+            System.out.println("Usuario cadastrado");
+            response.sendRedirect("login.jsp"); // Redirecionar para a página do menu
+
+        } else {
+            System.out.println("Erro no cadastro");
+            request.setAttribute("erroCadastro", "Erro ao cadastrar usuário. Verifique se os dados estão corretos.");
+            request.getRequestDispatcher("cadastro.jsp").forward(request, response);
+        }
+    }
+}
